3ec5cc9123ff8518f45d6ec2faa35129943b2a44
qiukey <943451277@qq.com>
2019-08-16 14:40:54 +0800

    qk

--- src/main/java/com/qk/community/IndexController.java
+++ src/main/java/com/qk/community/IndexController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 
 @Controller
-public class GreetingController {
+public class IndexController {
 
-    @GetMapping("/greeting")
+    @GetMapping("/")
     public String greeting(@RequestParam(name="name",required = false,defaultValue = "World") String name,Model model){
         model.addAttribute("name", name);
-        return "greeting";
+        return "index";
     }
 }
 
