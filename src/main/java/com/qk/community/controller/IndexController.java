fae90e0682e6456fcacdaab3e632e0de7bf6872a
qiukey <943451277@qq.com>
2019-09-12 15:01:07 +0800

    qk

--- src/main/java/com/qk/community/controller/IndexController.java
+++ src/main/java/com/qk/community/controller/IndexController.java
@@ -1,16 +1,42 @@
 package com.qk.community.controller;
 
+import com.qk.community.mapper.UserMapper;
+import com.qk.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
+    @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name",required = false,defaultValue = "World") String name,Model model){
         model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @Autowired
+    private UserMapper userMapper;
+    @GetMapping("/")
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie:cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
         return "index";
     }
 }
---
+++ src/main/resources/db/migration/V2__Add_bio_col_to_user_table.sql
@@ -0,0 +1,1 @@
+ALTER TABLE User ADD bio varchar(256) NULL;
